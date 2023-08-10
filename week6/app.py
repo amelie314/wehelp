6ac679f7d522e14e7d2a92cfb2201b01bfa83427
amelie314 <qangela314@gmail.com>
2023-08-10 20:31:40 +0800

    week6_finished

--- week6/app.py
+++ week6/app.py
@@ -42,25 +42,26 @@ def signin():
     username = request.form.get('username')
     password = request.form.get('password')
     
-    cursor = db.cursor()
+    cursor = db.cursor(dictionary=True)
     
     cursor.execute("SELECT * FROM member WHERE username = %s AND password = %s", (username, password))
     user = cursor.fetchone()
     
     if user:
         session['authenticated'] = True  
-        session['user_id'] = user[0] 
-        session['username'] = user[2] 
+        session['user_id'] = user['id'] 
+        session['username'] = user['username'] 
+        session['name'] = user['name']
         return redirect(url_for('member'))
     else:
         return redirect(url_for('error', message='帳號或密碼輸入錯誤'))
 
-@app.route('/member')
-def member():
-    if session.get('authenticated'):
-        return render_template('member.html', username=session.get('username'))
-    else:
-        return redirect(url_for('index'))
+# @app.route('/member')
+# def member():
+#     if session.get('authenticated'):
+#         return render_template('member.html', username=session.get('username'))
+#     else:
+#         return redirect(url_for('index'))
     
 @app.route('/error')
 def error():
@@ -72,5 +73,85 @@ def signout():
     session['authenticated'] = False
     return redirect(url_for('index'))
 
+
+@app.route('/create_message', methods=['POST'])
+def create_message():
+    if session.get('authenticated'):
+        user_id = session.get('user_id')
+        content = request.form.get('content')
+        
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO message (member_id, content) VALUES (%s, %s)", (user_id, content))
+        db.commit()
+        
+        return redirect(url_for('member'))
+    else:
+        return redirect(url_for('index'))
+
+# @app.route('/member')
+# def member():
+#     if session.get('authenticated'):
+        
+#         cursor = db.cursor(dictionary=True)
+
+#         query = """
+#         SELECT member.name, message.content, message.time, message.id as message_id
+#         FROM message
+#         INNER JOIN member ON message.member_id = member.id
+#         ORDER BY message.time DESC
+#         """
+
+#         cursor.execute(query)
+
+#         messages = cursor.fetchall()
+        
+#         return render_template('member.html', username=session.get('username'), messages=messages, user_id=session['user_id'])
+#     else:
+#         return redirect(url_for('index'))
+@app.route('/member')
+def member():
+    if session.get('authenticated'):
+        
+        cursor = db.cursor(dictionary=True)
+
+        # 修改的 SQL 查詢，新增 message.id, message.member_id 和 message.time
+        query = """
+        SELECT message.id, member.name, message.content, message.member_id, message.time
+        FROM message
+        INNER JOIN member ON message.member_id = member.id
+        ORDER BY message.time DESC
+        """
+
+        cursor.execute(query)
+
+        messages = []
+        for message_row in cursor.fetchall():
+            message = {
+                'id': message_row['id'],
+                'name': message_row['name'], 
+                'content': message_row['content'],
+                'member_id': message_row['member_id'],
+                'time': message_row['time'].strftime('%Y-%m-%d %H:%M:%S')  # 格式化時間
+            }
+            messages.append(message)
+        
+        return render_template('member.html', username=session.get('username'),messages=messages, name=session.get('name'))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/delete_message', methods=['POST'])
+def delete_message():
+    if session.get('authenticated'):
+        message_id = request.form.get('message_id')
+        
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM message WHERE id = %s", (message_id,))
+        db.commit()
+        
+        return redirect(url_for('member'))
+    else:
+        return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
